d558a5cd34ef5a8e4f76644508ea0cb94f4a6f76
Sanya Sachdeva <66810185+Sanyasachdevaa@users.noreply.github.com>
2022-11-17 16:51:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Searchbox from "./components/searchbox"
+import './App.css';
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1 >
+        Search Users on GitHub
+      </h1>
+      <Searchbox />
+    </main>
+  )
+}
+
+export default IndexPage
